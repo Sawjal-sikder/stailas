144b74b084f44b185f430816b00e8d8acaf265a3
sajal <mdsajalsikder@gmail.com>
2025-06-12 10:23:37 +0600

    for Success after set new password ui design

---
+++ src/component/SuccessComp.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "./Image";
+import Logo from "../assets/image/group 48.png"
+
+const SuccessComp = () => {
+  return (
+    <div className="mt-10 py-6 px-10 text-primary font-inter w-full">
+      <h2 className="text-xl sm:text-5xl font-bold pb-4">Password Reset</h2>
+      <p className="text-sm sm:text-sm font-extralight">
+        Your password has been successfully reset. click confirm to set a new
+        password
+      </p>
+      <div className="flex justify-center items-center py-20">
+            <Image
+              src={Logo}
+              alt="Logo"
+              className="w-40"
+            />
+      </div>
+    </div>
+  );
+};
+
+export default SuccessComp;
