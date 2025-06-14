38a7dd49d9e4902b695dd18f24b7db5893004b5f
sajal <mdsajalsikder@gmail.com>
2025-06-14 11:09:35 +0600

    fixed cart section

--- src/pages/home/SingleCart/Cart.jsx
+++ src/pages/home/SingleCart/Cart.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import Header from '../../../component/Header'
-import Image from '../../../component/Image'
-import CartImage from "../../../assets/image/service/cart.png"
-import Button from '../../../component/Button'
+import React from "react";
+import Header from "../../../component/Header";
+import Image from "../../../component/Image";
+import CartImage from "../../../assets/image/service/cart.png";
+import Button from "../../../component/Button";
 
 const Cart = () => {
   return (
-    <div className='px-40 py-20'>
-      <Header className='pr-20 pb-20' text={`For Women Who Want to Feel More “Put Together” Without the Effort`} />
-      <div className='grid grid-cols-2 items-center gap-x-10'>  
-        <div>
-            <Image src={CartImage} alt={"Cart Image"} />
+    <div className="px-4 lg:px-40 py-2">
+      <Header
+        className="pr-20 lg:pb-20"
+        text={`For Women Who Want to Feel More “Put Together” Without the Effort`}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-x-10">
+        {/* On small screens, reverse order using flex-col-reverse */}
+        <div className="order-2 md:order-1">
+          <Image src={CartImage} alt={"Cart Image"} className="pt-5" />
+          <Button
+            text="Start With One Outfit"
+            className="lg:hidden my-4 text-center block mx-auto"
+          />
         </div>
-        <div className='font-inter text-primary'>
-            <p className='text-lg pr-14'>Whether you’re back in the office, starting over, chasing a toddler, or just tired of second-guessing your look—Stailas is here to help you glow up quietly.</p>
-            <p className='text-lg pt-5'> No pressure. Just practical, beautiful style—your way.</p>
-            <Button text="Start With One Outfit" className="w-96 mt-10 text-xl" />
-
+        <div className="font-inter text-primary order-1 md:order-2">
+          <p className="text-[12px] lg:text-[20px] py-4 pr-0 md:pr-14">
+            Whether you’re back in the office, starting over, chasing a toddler,
+            or just tired of second-guessing your look—Stailas is here to help
+            you glow up quietly.
+          </p>
+          <p className="text-[12px] lg:text-[20px] lg:pt-5">
+            No pressure. Just practical, beautiful style—your way.
+          </p>
+          <Button
+            text="Start With One Outfit"
+            className="hidden lg:block lg:w-[345px] mt-10 p-5"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
