70076ad57c564864569a3bf643f8f5af4963566f
sajal <mdsajalsikder@gmail.com>
2025-06-14 10:47:27 +0600

    fixed responsive design for  what you''ll get

--- src/pages/home/getService/Items.jsx
+++ src/pages/home/getService/Items.jsx
@@ -8,27 +8,27 @@ import ServiceImage4 from "../../../assets/image/service/s4.png"
 
 const Items = () => {
   return (
-    <div className='grid grid-cols-4 gap-x-10 pt-10 '> 
+    <div className='grid grid-cols-2 lg:grid-cols-4 gap-4 pt-10 '> 
     {/* item */}
       <div className='p-5 text-center flex flex-col items-center border border-[#E5E5E5] rounded-md'>
-        <Image src={ServiceImage1} alt="Service Image 1" className="" />
-        <p className='font-bold text-xl py-4'>Style Profile</p>
-        <p className='text-md'>Color palette, signature vibes, and pro insights</p>
+        <Image src={ServiceImage1} alt="Service Image 1" className="w-[36px] lg:w-[88px] py-5" />
+        <p className='font-bold text-[16px] lg:text-[20px] py-2'>Style Profile</p>
+        <p className='text-[12px] lg:text-[16px]'>Color palette, signature vibes, and pro insights</p>
       </div>
       <div className='p-5 text-center flex flex-col items-center border border-[#E5E5E5] rounded-md'>
-        <Image src={ServiceImage2} alt="Service Image 1" className="" />
-        <p className='font-bold text-xl py-4'>Wardrobe Review</p>
-        <p className='text-md'>Suggestions on what to keep,how to wear it</p>
+        <Image src={ServiceImage2} alt="Service Image 1" className="w-[36px] lg:w-[88px] py-5" />
+        <p className='font-bold text-[16px] lg:text-[20px] py-2'>Wardrobe Review</p>
+        <p className='text-[12px] lg:text-[16px]'>Suggestions on what to keep,how to wear it</p>
       </div>
       <div className='p-5 text-center flex flex-col items-center border border-[#E5E5E5] rounded-md'>
-        <Image src={ServiceImage3} alt="Service Image 1" className="" />
-        <p className='font-bold text-xl py-4'>Smart Shopping</p>
-        <p className='text-md'>AI advice before you buy</p>
+        <Image src={ServiceImage3} alt="Service Image 1" className="w-[36px] lg:w-[88px] py-5" />
+        <p className='font-bold text-[16px] lg:text-[20px] py-2'>Smart Shopping</p>
+        <p className='text-[12px] lg:text-[16px]'>AI advice before you buy</p>
       </div>
       <div className='p-5 text-center flex flex-col items-center border border-[#E5E5E5] rounded-md'>
-        <Image src={ServiceImage4} alt="Service Image 1" className="" />
-        <p className='font-bold text-xl py-4'>Confidence Boost</p>
-        <p className='text-md'>Feel polished, not overwhelmed</p>
+        <Image src={ServiceImage4} alt="Service Image 1" className="w-[36px] lg:w-[88px] py-5" />
+        <p className='font-bold text-[16px] lg:text-[20px] py-2'>Confidence Boost</p>
+        <p className='text-[12px] lg:text-[16px]'>Feel polished, not overwhelmed</p>
       </div>
     </div>
   )
--- src/pages/home/getService/Service.jsx
+++ src/pages/home/getService/Service.jsx
@@ -4,10 +4,10 @@ import Items from './Items'
 
 const Service = () => {
     return (
-        <div className='py-20 px-40 font-inter text-primary'>
+        <div className='py-20 px-4 lg:px-40 font-inter text-primary'>
             <div className='pb-10'>
                 <Header text="What You’ll Get" className="mb-4" />
-                <p className='text-md'>Think of it as the style refresh you’ve always wanted. No
+                <p className='text-[12px] lg:text-[20px]'>Think of it as the style refresh you’ve always wanted. No
                     drama, no doubt—just clear, easy wins.</p>
             </div>
             <Items />
