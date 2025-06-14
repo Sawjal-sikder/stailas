2e73ea3f94911dd0e756c64b2b803295af00727d
sajal <mdsajalsikder@gmail.com>
2025-06-14 08:58:10 +0600

    button fixed issue

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ text, type = 'button', className = '' }) => {
   return (
     <button
       type={type}
-      className={`w-full text-white bg-secondary font-inter focus:ring-4 focus:outline-none focus:ring-primary/50 font-bold rounded-md text-md px-5 py-3 text-center ${className}`}
+      className={`text-white bg-secondary font-inter focus:ring-4 focus:outline-none focus:ring-primary/50 font-bold rounded-md text-md px-5 py-3 text-center ${className}`}
     >
       {text}
     </button>
