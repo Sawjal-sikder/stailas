19aadf913390dc6ddad9bbb66679773a57a3b80c
sajal <mdsajalsikder@gmail.com>
2025-06-28 12:57:33 +0600

    Refactor PasswordVerifyForm: replace Button component with a native button element for improved semantics and accessibility

--- src/component/PasswordVerifyForm.jsx
+++ src/component/PasswordVerifyForm.jsx
@@ -72,7 +72,13 @@ const PasswordVerifyForm = ({ email }) => {
           />
         </div>
 
-        <Button text="VERIFY" type="submit" />
+        <button
+          type='submit'
+          className="text-white text-[12px] lg:text-[16px] bg-secondary font-inter focus:ring-none focus:outline-none focus:ring-primary/50 font-bold text-md px-5 py-3 text-center"
+          disabled={code.otp_code.length !== 6}
+        >
+          VERIFY
+        </button>
       </form>
 
       <div className="flex justify-center items-center gap-x-1 *:text-sm">
